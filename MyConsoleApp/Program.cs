a2d282cbd3f3f0614545aa52b64738feb583ffb9
Dalia Abo Sheasha <daliasheasha@gmail.com>
2024-09-22 14:40:19 -0400

    Migrating to .NET 8

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,33 +1,34 @@
-﻿using System.Net;
+[EXPLANATION START] This is the changes needed to be made in order to migrate the app from .NET 6 to .NET 8:
+- Change `HttpWebRequest` to `HttpClient` for making the HTTP request.
+- Replace `StreamReader` with `HttpContent.ReadAsStringAsync()` to read the response.
+
+The corrected file is as follows:
+[EXPLANATION END] [CORRECTED FILE START]
+using System;
+using System.Net.Http;
 
 namespace WebRequestSample
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             string url = "https://jsonplaceholder.typicode.com/todos/1";
 
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.Method = "GET";
-            request.ContentType = "application/json";
+            using HttpClient client = new HttpClient();
+            using HttpResponseMessage response = await client.GetAsync(url);
 
-            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            if (response.IsSuccessStatusCode)
             {
-                if (response.StatusCode == HttpStatusCode.OK)
-                {
-                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
-                    {
-                        string responseText = reader.ReadToEnd();
-                        Console.WriteLine("Response received:");
-                        Console.WriteLine(responseText);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"Error: {response.StatusCode}");
-                }
+                string responseText = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Response received:");
+                Console.WriteLine(responseText);
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
             }
         }
     }
 }
+[CORRECTED FILE END]
