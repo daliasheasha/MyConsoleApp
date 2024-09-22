a4a9b869109efb82ab102c34541d4ba8374b6923
Dalia Abo Sheasha <dabosheasha@microsoft.com>
2024-09-22 14:16:30 -0400

    Add project files.

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+
+namespace WebRequestSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define the URL to send a request to
+            string url = "https://jsonplaceholder.typicode.com/todos/1";
+
+            // Create the WebRequest instance
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            // Optional: Set some request properties
+            request.Method = "GET";
+            request.ContentType = "application/json";
+
+            // Get the response from the server
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                // Check if the status code is OK (200)
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    // Read the response stream
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string responseText = reader.ReadToEnd();
+                        Console.WriteLine("Response received:");
+                        Console.WriteLine(responseText);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode}");
+                }
+            }
+        }
+    }
+}
