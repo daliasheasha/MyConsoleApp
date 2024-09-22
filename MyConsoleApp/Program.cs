a7bc1507c741d694c4ccddd92d3547c5b4591fef
Dalia Abo Sheasha <daliasheasha@gmail.com>
2024-09-22 15:20:47 -0400

    Migrating to .NET 8

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,4 +1,36 @@
-﻿using System.Net;
+[CORRECTED FILE START]using System.Net;
+
+namespace WebRequestSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://jsonplaceholder.typicode.com/todos/1";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string responseText = reader.ReadToEnd();
+                        Console.WriteLine("Response received:");
+                        Console.WriteLine(responseText);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode}");
+                }
+            }
+        }
+    }
+}[CORRECTED FILE END]using System.Net;
 
 namespace WebRequestSample
 {
