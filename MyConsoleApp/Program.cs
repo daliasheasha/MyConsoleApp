61416f04ca79370b3c0b17fa52d4524625a39986
Dalia Abo Sheasha <daliasheasha@gmail.com>
2024-09-22 14:27:49 -0400

    Migrating to .NET 8

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,24 +1,30 @@
-﻿using System.Net;
+Since you have requested to migrate this app to .NET 8, I assume there are some changes required for this migration. Below is the updated code for the given file:
+
+```csharp
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace WebRequestSample
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             string url = "https://jsonplaceholder.typicode.com/todos/1";
 
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            var request = (HttpWebRequest)WebRequest.Create(url);
             request.Method = "GET";
             request.ContentType = "application/json";
 
-            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
             {
                 if (response.StatusCode == HttpStatusCode.OK)
                 {
                     using (StreamReader reader = new StreamReader(response.GetResponseStream()))
                     {
-                        string responseText = reader.ReadToEnd();
+                        string responseText = await reader.ReadToEndAsync();
                         Console.WriteLine("Response received:");
                         Console.WriteLine(responseText);
                     }
@@ -31,3 +37,6 @@ namespace WebRequestSample
         }
     }
 }
+```
+
+Please replace the existing code with the updated code provided above.
